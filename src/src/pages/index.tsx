7767e0e3d5ec7a5d20cd61c9438e7c1b0403b727
mitate-gengaku <mitategengaku@gmail.com>
2024-09-13 14:09:31 +0900

    fix: fix write ogp.png

--- src/src/pages/index.tsx
+++ src/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { createCanvas } from "@napi-rs/canvas";
+import { writeFile, writeFileSync } from "fs";
 import Head from "next/head";
 import Image from "next/image";
+import path from "path";
+import { cwd } from "process";
 
 interface Props {
   url: string;
-  imgUrl: string;
 }
 
-const Home = ({ url, imgUrl }: Props) => {
+const Home = ({ url }: Props) => {
   return (
     <>
       <Head>
@@ -25,11 +27,11 @@ const Home = ({ url, imgUrl }: Props) => {
         <meta property="og:type" content={"website"} />
         <meta property="og:url" content={url} />
         <meta property="og:site_name" content={"OGPの生成の練習"} />
-        <meta property="og:image" content={imgUrl} />
+        <meta property="og:image" content={"/ogp.png"} />
 
         <meta name="twitter:title" content={"OGPの生成の練習"} />
         <meta name="twitter:description" content={"OGPの生成の練習"} />
-        <meta name="twitter:image" content={imgUrl} />
+        <meta name="twitter:image" content={"/ogp.png"} />
 
         <link
           rel="canonical"
@@ -38,7 +40,7 @@ const Home = ({ url, imgUrl }: Props) => {
       </Head>
       <div>
         <Image 
-          src={imgUrl}
+          src={"/ogp.png"}
           alt="画像"
           width={1200}
           height={630}
@@ -60,12 +62,19 @@ export const getStaticProps = async () => {
   ctx.textBaseline = 'top';
   ctx.textAlign = 'center';
 
-  ctx.fillText("Hello World", 500, 100)
+  ctx.fillText("Hello World", 500, 100);
+  const url = canvas.toDataURL("image/png");
+  const image = url.split(";base64,").pop();
+
+  if(!image) return;
+
+  writeFileSync(path.join(cwd(), "public", 'ogp.png'), image, {
+    encoding: "base64"
+  });
 
   return {
     props: {
       url: process.env.BASE_URL,
-      imgUrl: canvas.toDataURL("image/png")
     }
   }
 }
