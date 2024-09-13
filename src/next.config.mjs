c970768ca00fbee96610981d75e2c5036911290e
mitate-gengaku <mitategengaku@gmail.com>
2024-09-13 16:31:34 +0900

    fix: fix display ogp.png

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'generate-ogp-images-with-ssg.vercel.app'
+    ]
+  }
 };
 
 export default nextConfig;
